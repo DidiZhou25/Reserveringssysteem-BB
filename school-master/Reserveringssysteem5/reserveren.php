e65424090de700a3ed5a8993d610b00f5b726198
DidiZhou25 <didizhou25@hotmail.com>
2018-06-24 22:59:46 +0200

    Add files via upload

---
+++ school-master/Reserveringssysteem5/reserveren.php
@@ -0,0 +1,14 @@
+<form method="post" action="database.php">
+  <input type="text" name="sex" placeholder="sex">
+  <input type="text" name="kind" placeholder="kind">
+  <input type="text" name="datepicker" placeholder="datepicker">
+  <input type="text" name="day_time" placeholder="day_time">
+  <input type="text" name="bsn_number" placeholder="bsn_number">
+  <input type="text" name="address" placeholder="address">
+  <input type="text" name="residence" placeholder="residence">
+  <input type="text" name="postal_code" placeholder="postal_code">
+  <input type="text" name="birthplace" placeholder="birthplace">
+  <input type="text" name="phone_number" placeholder="phone_number">
+  <input type="text" name="notes" placeholder="notes">
+  <input type="submit">
+</form>
